bbafdc3b69387675df2df226950ec38571d93a35
Vaibhav Rangare <47136076+VaibhavRangare@users.noreply.github.com>
2020-08-16 13:33:41 +0530

    Add files via upload

---
+++ spring-rest/src/main/java/com/vaibhav/spring/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.vaibhav.spring.dao;
+
+import java.util.List;
+
+import com.vaibhav.spring.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
